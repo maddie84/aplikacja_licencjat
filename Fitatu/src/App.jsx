eafe0c1f52b87afd4dddad3bdaf5298b64b72739
„maddie84 <jazwinskamagda@gmail.com>
2023-03-18 16:03:58 +0100

    auth-form

--- Fitatu/src/App.jsx
+++ Fitatu/src/App.jsx
@@ -1,16 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import { firestore } from './firebase-config/firestore';
-import { doc, setDoc } from "firebase/firestore";
 import Auth from './auth';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const Push = async() => {
-    await setDoc(doc(firestore, "board", "tasks"), {
-      count
-  });
-  }
   return (
     <Auth/>
   )
--- Fitatu/src/auth/index.jsx
+++ Fitatu/src/auth/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+// kod, który piszę jest semantyczny co oznacza, że jest przyjazny osobom niepełnosprawnym, które wykorzystują narzędzia
+//potrzebne do odczytu ekranu, z którymi nasz kod jest kompatybilny 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <form><label htmlFor='email'> Email </label> 
+    <input id='email' name='email'/> 
+    <label htmlFor='password'> Password </label>
+    <input id='password' name='password'/>
+    <label htmlFor='repeatPassword'> Repeat Password </label>
+    <input id='repeatPassword' name='repeatPassword'/>
+    <button> Submit </button>
+    </form>
   )
 }
 
